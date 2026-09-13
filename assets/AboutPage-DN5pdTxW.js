85228239c0440c8f0ba20696f1c181fa826e0374
19359cyh <ccchang0722@gmail.com>
2026-09-13 21:05:23 +0800

    Deploy

---
+++ assets/AboutPage-DN5pdTxW.js
@@ -0,0 +1,12 @@
+import{r as e,t}from"./vendor-react-FpmXRWTi.js";import{t as n}from"./Menu-CBXqEle0.js";var r=[`在無聲的山岳深處，回蕩著未曾終止的律動。`,`我們，是踏入樂音洪流的旅人。
+音符不再只是冷冽的符號，而是脈動的光子。`,`每一次下落，皆如星辰墜落大地；
+每一次擊打，便能使靈魂與宇宙之能共鳴。`,`這並非單純的遊戲，而是一場向未知延伸的探索——
+對未來科技的試煉，對美學感知的挑戰。
+當節奏化為洪潮，判定化作斷崖，曲線隱匿幻影之時，
+皆在呼喚那些願意迎接挑戰之人。`,`若你願意，請跨越門檻。
+在此之後，你所踏入的，不再只是遊戲，而是`,`一場以「音」為名的試煉。`,`
+以<b>「北」</b>極星之名
+使<b>「藝」</b>之魂閃耀於長空
+劃開交織<b>「音」</b>符的長河
+<b>「遊」</b>歷於屬於自己的軌跡`],i=t();function a(){return(0,i.jsxs)(`div`,{className:`about-page`,children:[(0,i.jsx)(e,{to:`/home`,className:`logo-link`,children:(0,i.jsx)(`img`,{src:`/img/mg2.png`,className:`mg-logo`,alt:`TMGC Logo`})}),(0,i.jsx)(n,{}),(0,i.jsx)(`div`,{className:`about`,children:(0,i.jsx)(`p`,{children:r.map((e,t)=>(0,i.jsxs)(`span`,{children:[e.split(`
+`).map((e,t)=>(0,i.jsx)(`span`,{dangerouslySetInnerHTML:{__html:e+`<br />`}},t)),t<r.length-1&&(0,i.jsx)(`br`,{})]},t))})}),(0,i.jsx)(`footer`,{className:`site-footer`,children:`© TNUA MUSIC GAME CLUB 2nd`})]})}export{a as default};
