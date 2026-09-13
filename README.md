3a261fa11e5a4e77f6b830965b9a3be542effbf9
19359cyh <ccchang0722@gmail.com>
2026-09-13 18:56:06 +0800

    fin4

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
-北藝音遊
+<h1 align="center" style="font-size: 4em; color: #000; -webkit-text-stroke: 1px #4FC0C4;"><b>北 藝 音 遊</b></h1>
 
-# 活動用json加，記得放照片進資料夾（沒後端）
-# ig貼分享網址
-後面看到 ig_web_copy_link&stkn=MzRlODBiNWFlZA== 之類的
-改成 ig_embed&utm_campaign=loading
+***
+## HOME
+### - ig貼分享網址
+  後面看到 ig_web_copy_link&stkn=MzRlODBiNWFlZA== 之類的<br>
+  改成 ig_embed&utm_campaign=loading
 
-# member如果要有不同版本圖
-"imgDesktop": "/img/member/---.png",
-"imgMobile": "/img/member/---.png",
+***
+## ABOUT
+### - 改介紹改json
+* 換行打 `\n`，文字加粗用 `<b>字</b>`
+
+***
+## MEMBER
+### - 資料夾路徑在 ./public/img/member/
+* 換幹部直接改同檔名取代資料夾內圖片就ok
+
+### - member如果要有不同版本圖
+* "imgDesktop": "/img/member/---.png",
+* "imgMobile": "/img/member/---.png",
+
+***
+## ACTIVITY
+### - 活動用json加，記得放照片進 ./public/img/activity/ 資料夾（沒後端）
+
+***
+## CONTACT
+### - 要改直接改socialLinks.jsx
--- src/view/pages/AboutPage.jsx
+++ src/view/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import Menu from '../menu/Menu'
+import aboutData from '../../json/about.json'
 import './AboutPage.css'
 
 export default function AboutPage() {
@@ -13,25 +14,17 @@ export default function AboutPage() {
 
       <div className="about">
         <p>
-          在無聲的山岳深處，回蕩著未曾終止的律動。<br />
-          <br />
-          我們，是踏入樂音洪流的旅人。<br />
-          音符不再只是冷冽的符號，而是脈動的光子。<br /><br />
-          每一次下落，皆如星辰墜落大地；<br />
-          每一次擊打，便能使靈魂與宇宙之能共鳴。<br />
-          <br />
-          這並非單純的遊戲，而是一場向未知延伸的探索——<br />
-          對未來科技的試煉，對美學感知的挑戰。<br />
-          當節奏化為洪潮，判定化作斷崖，曲線隱匿幻影之時，<br />
-          皆在呼喚那些願意迎接挑戰之人。<br />
-          <br />
-          若你願意，請跨越門檻。<br />
-          在此之後，你所踏入的，不再只是遊戲，而是<br /><br />
-          一場以「音」為名的試煉。<br />
-          以<b>「北」</b>極星之名<br />
-          使<b>「藝」</b>之魂閃耀於長空<br />
-          劃開交織<b>「音」</b>符的長河<br />
-          <b>「遊」</b>歷於屬於自己的軌跡
+          {aboutData.map((paragraph, i) => (
+            <span key={i}>
+              {paragraph.split('\n').map((line, j) => (
+                <span
+                  key={j}
+                  dangerouslySetInnerHTML={{ __html: line + '<br />' }}
+                />
+              ))}
+              {i < aboutData.length - 1 && <br />}
+            </span>
+          ))}
         </p>
       </div>
 
